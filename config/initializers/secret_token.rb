33fc292fdd709da316a86024a34f06c55b71d4d9
Jagadeesh Gajula <jagadish.d.one@gmail.com>
2013-09-30 21:39:21 -0700

    asdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hindu::Application.config.secret_token = 'd2a477d763480ce44c21aa0154dc6765c2f98ffbc18268b5ecfcaf298b5f2a464c36cc0c2f17a23e21ff72cd6bc139ca13c985d03b3efc6381ff24b9790f92bb'
